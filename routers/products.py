88308503ef8826f61dcd89dd72ee5d297d8200a4
Dami <damiarmoa@gmail.com>
2026-05-20 10:33:09 -0300

    Agrego ruta de checkout

--- routers/products.py
+++ routers/products.py
@@ -1,12 +1,41 @@
 from fastapi import APIRouter, HTTPException, Depends 
 from sqlalchemy.orm import Session 
-from schemas.products import Product, Id_Product
-from models.products import ProductDB 
+from schemas.products import Product, Id_Product, OrderCheckOut, PurchaseScheme
+from models.products import ProductDB
 from database import get_db
 from routers.users import get_current_user
 from models.users import UserDB
 
 router = APIRouter(prefix="/products", tags=["Products"])
+
+@router.post("/checkout", summary = "Procesar carrito de compras")
+def process_cart(order : OrderCheckOut, db : Session = Depends(get_db)): 
+    product_to_update = []
+
+    for item in order.items:
+        producto_db = db.query(ProductDB).filter(ProductDB.id == item.product_id).first()
+
+        if not producto_db or producto_db.count < item.quantity_purchased: 
+            raise HTTPException(status_code=400, detail=f"Falla de stock en el producto ID: {item.product_id}")
+    
+        producto_db.count -= item.quantity_purchased
+        product_to_update.append(producto_db)
+
+    db.commit()
+    return {"mensaje" : "¡Compra exitosa!", "processed articles" : len(order.items)}
+
+
+@router.post("/shopping", summary="Al comprar un producto bajara su stock")
+def make_purchase(buy : PurchaseScheme,db : Session = Depends(get_db)): 
+    product_db = db.query(ProductDB).filter(ProductDB.id == buy.product_id).first()
+
+    if product_db.count < buy.quantity_purchased: 
+        raise HTTPException(status_code=400, detail="No hay stock suficiente")
+    
+    product_db.count -= buy.quantity_purchased
+
+    db.commit()
+    return {"mensaje": "La compra ha sido exitosa"}
 
 @router.get("/{id}", response_model=Id_Product)
 async def get_product(id : int, db : Session = Depends(get_db)):
@@ -29,6 +58,9 @@ async def get_all_products(maximun_price : float | None = None, skip : int = 0, 
 @router.post("/", response_model=Id_Product, summary="Crea un producto nuevo (requiere token)", description="Crea un nuevo producto desde cero en el servidor.") 
 async def post_products(product: Product, db : Session = Depends(get_db), current_user : UserDB = Depends(get_current_user)):
     new_product_db = ProductDB(**product.model_dump(), owner_id = current_user.id)
+
+    if not current_user.is_admin: 
+        raise HTTPException(status_code=403, detail="No tienes permisos de administrador")
 
     db.add(new_product_db)
     db.commit() 
--- schemas/products.py
+++ schemas/products.py
@@ -5,7 +5,14 @@ class Product(BaseModel):
     price: float = Field(gt=0, description="El precio debe ser estrictamente mayor que 0")
     count: int = Field(ge=0)
 
-# Lo que nosotros devolveremos 
 class Id_Product(Product): 
     id: int
     model_config = {"from_attributes" : True}
+
+class PurchaseScheme(BaseModel): 
+    product_id : int 
+    quantity_purchased : int 
+
+class OrderCheckOut(BaseModel): 
+    items: list[PurchaseScheme] 
+    payment_method : str 
